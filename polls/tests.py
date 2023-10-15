bdaf7ebc34ae75c19344a1d652f0789bfefdb83c
himanshu1196 <11.himanshu.96@gmail.com>
2023-10-15 14:57:46 -0400

    Adding black, flake, coveralls

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
+from django.utils import timezone
+from polls.models import Question
+
 # Create your tests here.
+class QuestionTestCase(TestCase):
+    def setUp(self):
+        Question.objects.create(question_text = "Is this a test question?", pub_date = timezone.now())
+
+    def test_animals_can_speak(self):
+        """Animals that can speak are correctly identified"""
+        q = Question.objects.get(question_text = "Is this a test question?")
+        self.assertTrue(q is not None)
